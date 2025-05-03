6f708128a17dbb1ff1b6d21138e75d9e108f8a2c
LeeGwanWoo <pckpck4566@snu.ac.kr>
2025-05-03 13:47:56 +0900

    Professor & Report monster, need to change prefabs

---
+++ Assets/Scripts/ProfessorController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfessorController : MonoBehaviour
+{
+    private GameObject player;
+    public float speed = 40f;
+    public float attackRange = 100f;
+    public float attackDelay = 2f;
+    private bool isAttack;
+    private Vector3 playerPastPosition;
+    public float rotationSpeed = 5f;
+    
+    [SerializeField] private GameObject warningPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        Vector3 playerCurrentPosition = player.transform.position;
+
+        if ((currentPosition - playerCurrentPosition).sqrMagnitude < attackRange && !isAttack) {
+            Debug.Log("Attack Start");
+            isAttack = true;
+            StartCoroutine("attackRoutine");
+        }
+
+        Vector3 direction = playerPastPosition - currentPosition;
+        Quaternion targetRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+    }
+
+    IEnumerator attackRoutine() {
+        playerPastPosition = player.transform.position;
+        Instantiate(warningPrefab, player.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(attackDelay);
+        isAttack = false;
+    }
+}
