f42829cf42ef2acc2e9c513182f7528c5542676d
LeeGwanWoo <pckpck4566@snu.ac.kr>
2025-05-10 18:02:51 +0900

    Professor Attack Feature Complete

--- Assets/Scripts/PreviousProfessorController.cs
+++ Assets/Scripts/PreviousProfessorController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProfessorController : MonoBehaviour
+public class PreviousProfessorController : MonoBehaviour
 {
     private GameObject player;
     public float speed = 40f;
--- Assets/Scripts/ProfessorController.cs
+++ Assets/Scripts/ProfessorController.cs
@@ -1,46 +1,43 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProfessorController : MonoBehaviour
 {
-    private GameObject player;
-    public float speed = 40f;
-    public float attackRange = 100f;
-    public float attackDelay = 2f;
-    private bool isAttack;
-    private Vector3 playerPastPosition;
-    public float rotationSpeed = 5f;
-    
-    [SerializeField] private GameObject warningPrefab;
+    private bool facingForward = true;
 
-    // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        StartCoroutine(PatrolRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PatrolRoutine()
     {
-        Vector3 currentPosition = transform.position;
-        Vector3 playerCurrentPosition = player.transform.position;
+        while (true)
+        {
+            // 정지 시간: 2초 또는 1초
+            float waitTime = facingForward ? 2f : 1f;
+            yield return new WaitForSeconds(waitTime);
 
-        if ((currentPosition - playerCurrentPosition).sqrMagnitude < attackRange && !isAttack) {
-            Debug.Log("Attack Start");
-            isAttack = true;
-            StartCoroutine("attackRoutine");
+            // 현재 방향 기준으로 목표 회전 계산
+            Quaternion startRot = transform.rotation;
+            Quaternion endRot = startRot * Quaternion.Euler(0f, 180f, 0f);
+
+            // 회전 시간과 속도 설정
+            float rotateDuration = 0.2f;
+            float elapsed = 0f;
+
+            while (elapsed < rotateDuration)
+            {
+                transform.rotation = Quaternion.Slerp(startRot, endRot, elapsed / rotateDuration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // 정확히 도달하지 못할 경우를 위해 마지막 값 보정
+            transform.rotation = endRot;
+
+            // 다음 단계에서 반대 방향 대기 시간 사용하도록 플래그 토글
+            facingForward = !facingForward;
         }
-
-        Vector3 direction = playerPastPosition - currentPosition;
-        Quaternion targetRotation = Quaternion.LookRotation(direction.normalized, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-    }
-
-    IEnumerator attackRoutine() {
-        playerPastPosition = player.transform.position;
-        Instantiate(warningPrefab, player.transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(attackDelay);
-        isAttack = false;
     }
 }
