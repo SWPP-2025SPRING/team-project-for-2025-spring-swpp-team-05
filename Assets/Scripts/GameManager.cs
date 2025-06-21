a9df4ef25dbacf5652c23a181c14c895198e68d1
Kwack Minseo <163567726+hometogo4ever@users.noreply.github.com>
2025-06-21 16:54:18 +0900

    Revert "Feature: UI 개선하기"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -43,9 +43,12 @@ public class GameManager : MonoBehaviour
             elapsedTime += Time.deltaTime;
 
             float speed = PlayerStatus.instance.moveSpeed * 3.6f;
+            // TODO: 공격 스탯 필요없다는 결론 나오면 다 삭제
+            float atkRatio = PlayerStatus.instance.GetAttackPowerRatio();
+            float rangeRatio = PlayerStatus.instance.GetAttackRangeRatio();
 
             uiManager.UpdateTimer(elapsedTime);
-            uiManager.UpdateStats(speed);
+            uiManager.UpdateStats(speed, atkRatio, rangeRatio);
         }
     }
 
